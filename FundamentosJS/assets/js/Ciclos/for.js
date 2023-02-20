f27ac0449ad118d7e1b72d261c65422342f2f0c2
alexandro.berdejo <alexandro.berdejo@gmail.com>
2023-02-19 22:01:39 -0600

    'FinFundamentosInicioBlackjack

---
+++ FundamentosJS/assets/js/Ciclos/for.js
@@ -0,0 +1,16 @@
+const heroes = ['batman', 'loki', 'superman', 'spiderman'];
+
+console.warn('for tradicional');
+for ( let i = 0; i < heroes.length; i++  ) {
+    console.log ( heroes[i] );
+}
+
+console.warn ('for in');
+for (let i in heroes )  {
+    console.log( heroes[i] );
+}
+
+console.warn ('for of');
+for (let heroe of heroes){
+    console.log(heroe)
+}
