eede874a60ddc234d47ba67315dfa16ea48f4743
alexandro.berdejo <alexandro.berdejo@gmail.com>
2023-02-24 23:21:02 -0600

    'empezovoteyserefactorizocodigo'

---
+++ blackjack2-vite/main.js
@@ -0,0 +1,10 @@
+import './src/blackjack/index'
+import './style.css'
+
+
+
+
+
+
+
+
---
+++ blackjack2-vite/src/blackjack/usecases/index.js
@@ -0,0 +1,5 @@
+export { crearCartaHTML } from './crear-deck';
+export { crearDeck } from './crear-carta.html';
+export { pedirCarta } from './pedir-carta';
+export { turnoComputadora } from './turno-computadora';
+export { valorCarta } from './valor-Carta';
